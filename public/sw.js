1cdb0498d27afc03de51e1c3914089e7b6b43fe5
Jay T <jaytula@gmail.com>
2019-09-14 23:44:25 -0700

    Change active to activate

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@ self.addEventListener("install", function(event) {
   console.log("[Service Worker] Installing Server Worker ...", event);
 });
 
-self.addEventListener("active", function(event) {
+self.addEventListener("activate", function(event) {
   console.log("[Service Worker] Activating Server Worker ...", event);
   return self.clients.claim();
 });
