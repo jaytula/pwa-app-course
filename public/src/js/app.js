cbfd3699a73eeae67e8ae7e7e5ee6a3dae4d9e63
Jay T <jaytula@gmail.com>
2019-09-15 11:26:56 -0700

    Section 3 complete

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,15 @@
+var deferredPrompt;
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js").then(function() {
     console.log("Service worker registered!");
   });
 }
+
+window.addEventListener("beforeinstallprompt", function(event) {
+  console.log("beforeinstallprompt fired");
+  // event.preventDefault no longer has an effect after Chrome 68 with the Mini-infobar
+  // https://developers.google.com/web/updates/2018/06/a2hs-updates
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
