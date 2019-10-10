d5b277c29b6a1e4f578834089243dcdf76f044b3
Jay T <jaytula@gmail.com>
2019-10-10 16:42:14 -0700

    Appears to work more consitently with these changes but don't know why

--- main/public/src/js/feed.js
+++ main/public/src/js/feed.js
@@ -1,74 +1,82 @@
-var shareImageButton = document.querySelector("#share-image-button");
-var createPostArea = document.querySelector("#create-post");
-var closeCreatePostModalButton = document.querySelector(
-  "#close-create-post-modal-btn"
-);
-var sharedMomentsArea = document.querySelector("#shared-moments");
+var shareImageButton = document.querySelector('#share-image-button');
+var createPostArea = document.querySelector('#create-post');
+var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var sharedMomentsArea = document.querySelector('#shared-moments');
 
 function openCreatePostModal() {
-  createPostArea.style.display = "block";
+  createPostArea.style.display = 'block';
   if (deferredPrompt) {
     deferredPrompt.prompt();
 
     deferredPrompt.userChoice.then(function(choiceResult) {
       console.log(choiceResult.outcome);
 
-      if (choiceResult.outcome === "dismissed") {
-        console.log("User cancelled installation");
+      if (choiceResult.outcome === 'dismissed') {
+        console.log('User cancelled installation');
       } else {
-        console.log("User added to home screen");
+        console.log('User added to home screen');
       }
     });
 
     deferredPrompt = null;
   }
+
+  // if ('serviceWorker' in navigator) {
+  //   navigator.serviceWorker.getRegistrations()
+  //     .then(function(registrations) {
+  //       for (var i = 0; i < registrations.length; i++) {
+  //         registrations[i].unregister();
+  //       }
+  //     })
+  // }
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = "none";
+  createPostArea.style.display = 'none';
 }
 
-shareImageButton.addEventListener("click", openCreatePostModal);
+shareImageButton.addEventListener('click', openCreatePostModal);
 
-closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
+closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
-// Currently not in use
-function onSaveButtonClicked() {
-  console.log("clicked");
-  if ("caches" in window) {
-    caches.open("user-requested").then(function(cache) {
-      cache.add("https://httpbin.org/get");
-      cache.add("/src/images/sf-boat.jpg");
-    });
+// Currently not in use, allows to save assets in cache on demand otherwise
+function onSaveButtonClicked(event) {
+  console.log('clicked');
+  if ('caches' in window) {
+    caches.open('user-requested')
+      .then(function(cache) {
+        cache.add('https://httpbin.org/get');
+        cache.add('/src/images/sf-boat.jpg');
+      });
   }
 }
 
 function clearCards() {
-  while (sharedMomentsArea.hasChildNodes()) {
+  while(sharedMomentsArea.hasChildNodes()) {
     sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
   }
 }
 
 function createCard(data) {
-  var cardWrapper = document.createElement("div");
-  cardWrapper.className = "shared-moment-card mdl-card mdl-shadow--2dp";
-  var cardTitle = document.createElement("div");
-  cardTitle.className = "mdl-card__title";
-  cardTitle.style.backgroundImage = `url("${data.image}")`;
-  cardTitle.style.backgroundSize = "cover";
+  var cardWrapper = document.createElement('div');
+  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+  var cardTitle = document.createElement('div');
+  cardTitle.className = 'mdl-card__title';
+  cardTitle.style.backgroundImage = 'url(' + data.image + ')';
+  cardTitle.style.backgroundSize = 'cover';
   cardWrapper.appendChild(cardTitle);
-  var cardTitleTextElement = document.createElement("h2");
-  cardTitleTextElement.style.color = "yellow";
-  cardTitleTextElement.className = "mdl-card__title-text";
+  var cardTitleTextElement = document.createElement('h2');
+  cardTitleTextElement.style.color = 'white';
+  cardTitleTextElement.className = 'mdl-card__title-text';
   cardTitleTextElement.textContent = data.title;
   cardTitle.appendChild(cardTitleTextElement);
-  var cardSupportingText = document.createElement("div");
-  cardSupportingText.className = "mdl-card__supporting-text";
+  var cardSupportingText = document.createElement('div');
+  cardSupportingText.className = 'mdl-card__supporting-text';
   cardSupportingText.textContent = data.location;
-  cardSupportingText.style.textAlign = "center";
-  // var cardSaveButton = document.createElement("button");
-  // cardSaveButton.addEventListener("click", onSaveButtonClicked);
-  // cardSaveButton.textContent = "Save";
+  cardSupportingText.style.textAlign = 'center';
+  // var cardSaveButton = document.createElement('button');
+  // cardSaveButton.textContent = 'Save';
+  // cardSaveButton.addEventListener('click', onSaveButtonClicked);
   // cardSupportingText.appendChild(cardSaveButton);
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
@@ -77,15 +85,13 @@ function createCard(data) {
 
 function updateUI(data) {
   clearCards();
-
   for (var i = 0; i < data.length; i++) {
     createCard(data[i]);
   }
 }
 
-var url = "https://pwagram-9071a.firebaseio.com/posts.json";
+var url = 'https://pwagram-9071a.firebaseio.com/posts.json';
 var networkDataReceived = false;
-const DEBUG = false;
 
 fetch(url)
   .then(function(res) {
@@ -93,7 +99,7 @@ fetch(url)
   })
   .then(function(data) {
     networkDataReceived = true;
-    if (DEBUG) console.log("from web", data);
+    console.log('From web', data);
     var dataArray = [];
     for (var key in data) {
       dataArray.push(data[key]);
@@ -101,12 +107,12 @@ fetch(url)
     updateUI(dataArray);
   });
 
-if ("indexedDB" in window) {
+if ('indexedDB' in window) {
   readAllData('posts')
-  .then(function(data) {
-    if(!networkDataReceived) {
-      console.log('From cache', data);
-      updateUI(data);
-    }
-  })
+    .then(function(data) {
+      if(!networkDataReceived) {
+        console.log('From cache', data);
+        updateUI(data);
+      }
+    });
 }
--- main/public/sw.js
+++ main/public/sw.js
@@ -1,7 +1,7 @@
 importScripts('/src/js/idb.js');
 importScripts('/src/js/utility.js');
 
-var CACHE_STATIC_NAME = 'static-v17';
+var CACHE_STATIC_NAME = 'static-v2';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
 var STATIC_FILES = [
   '/',
@@ -21,15 +21,18 @@ var STATIC_FILES = [
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
 
-function trimCache(cacheName, maxItems) {
-  caches.open(cacheName).then(cache => {
-    return cache.keys().then(keys => {
-      if (keys.length > maxItems) {
-        return cache.delete(keys[0]).then(trimCache(cacheName, maxItems));
-      }
-    });
-  });
-}
+// function trimCache(cacheName, maxItems) {
+//   caches.open(cacheName)
+//     .then(function (cache) {
+//       return cache.keys()
+//         .then(function (keys) {
+//           if (keys.length > maxItems) {
+//             cache.delete(keys[0])
+//               .then(trimCache(cacheName, maxItems));
+//           }
+//         });
+//     })
+// }
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
@@ -70,11 +73,6 @@ function isInArray(string, array) {
   return array.indexOf(cachePath) > -1;
 }
 
-// a. At top level, we ultimately have event.respondWith.
-// b. We can have if/elseif/else and test against event.request.url to use different cache strategies
-// c. If here handles one particular url
-// d. The Else-if case here is a cache-only for those files in the STATIC_FILES list
-// e. The Else case is for dynamic caching.  Retrieves from cache if found and does a fetch-cache otherwise
 self.addEventListener('fetch', function(event) {
   var url = 'https://pwagram-9071a.firebaseio.com/posts';
   if (event.request.url.indexOf(url) > -1) {
@@ -90,7 +88,6 @@ self.addEventListener('fetch', function(event) {
               writeData('posts', data[key]);
             }
           });
-
         return res;
       })
     );
@@ -105,7 +102,6 @@ self.addEventListener('fetch', function(event) {
           return fetch(event.request)
             .then(function(res) {
               return caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
-                //trimCache(CACHE_DYNAMIC_NAME, 2);
                 cache.put(event.request.url, res.clone());
                 return res;
               });
